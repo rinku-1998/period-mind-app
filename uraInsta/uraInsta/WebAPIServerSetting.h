e9c350a8d42aa087197a7b3135eaac9f8349b851
phantomSTo <hero0223034676@gmail.com>
2020-01-06 23:15:12 +0800

    newcommentview autolayout
    nothing

--- uraInsta/uraInsta/WebAPIServerSetting.h
+++ uraInsta/uraInsta/WebAPIServerSetting.h
@@ -9,9 +9,9 @@
 #ifndef WebAPIServerSetting_h
 #define WebAPIServerSetting_h
 
-//#define SERVER_URL @"http://127.0.0.1:5000/api/"
-//#define SERVER_STATIC_URL @"http://127.0.0.1:5000/static/images/"
-#define SERVER_URL @"http://192.168.43.116:5000/api/"
-#define SERVER_STATIC_URL @"http://192.168.43.116:5000/static/images/"
+#define SERVER_URL @"http://127.0.0.1:5000/api/"
+#define SERVER_STATIC_URL @"http://127.0.0.1:5000/static/images/"
+//#define SERVER_URL @"http://192.168.43.116:5000/api/"
+//#define SERVER_STATIC_URL @"http://192.168.43.116:5000/static/images/"
 
 #endif /* WebAPIServerSetting_h */
